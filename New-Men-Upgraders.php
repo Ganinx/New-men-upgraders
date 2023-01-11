efe0703495d0aa735b5f351c43eb2ecbf845fdec
Jose <josemiguelvalcosta@gmail.com>
2023-01-11 16:38:50 +0100

    animation du bouton

--- New-Men-Upgraders.php
+++ New-Men-Upgraders.php
@@ -1,11 +1,10 @@
-<!DOCTYPE html>
 <html lang="en">
 <head>
     <link rel="preconnect" href="https://fonts.googleapis.com"><link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins&display=swap" rel="stylesheet">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New Men Upgraders</title>
+    <title>NewmenUprgraders</title>
     <link rel="stylesheet" href="New-Men-Upgraders.CSS">
 </head>
 <body>
