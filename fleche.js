d93cd75827b53355a63bf964cab3cbb77b326217
Moonbrite <josemiguelvalecosta@gmail.com>
2023-01-17 18:58:57 +0100

    fleche jaune  mfleche jaune

---
+++ fleche.js
@@ -0,0 +1,8 @@
+var flechejaune = document.getElementsByClassName("flechejaune") [0];
+
+flechejaune.addEventListener("click",function() {
+    window.scroll({
+        top:0,
+        behavior:"smooth"
+    });
+})
