6b9aa283b1b486a065c62a08376e54b398d7202c
ganinx <delomenie-v-87@hotmail.fr>
2023-01-26 13:34:03 +0100

    refonte, php, organisation des fichier

---
+++ Header.php
@@ -0,0 +1,17 @@
+<header>
+    <nav class="nav">
+        <ul class="container">
+            <img class="icone" src="images/Logo-nmu.png" alt="Logo des associés">
+            <li><a href=""><span class="t">NewMen</span><span class="b">Upgraders</span></a></li>
+            <div class="menu">
+                <li class="accueil"><a href="">Accueil </a></li>
+                <li class="cv"><a href="">Nos CV</a></li>
+                <li class="portef" ><a href="">Portefolio</a></li>
+            </div>
+            <div class="button">
+                <a class="btn" href="https://humanbooster.com/">Nous contacter </a>
+            </div>
+        </ul>
+    </nav>
+</header>
+
---
+++ Index.php
@@ -0,0 +1,56 @@
+<html lang="fr">
+<head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NewmenUprgraders</title>
+    <link rel="stylesheet" href="CSS/main.css">
+    <link rel="stylesheet" href="CSS/header.css">
+    <link rel="stylesheet" href="CSS/footer.css">
+</head>
+<body>
+    <?php
+    include "Header.php";
+    ?>
+    <section class="premiere" id="text1">
+        <div class="c">
+            <img class="tablette" src="images/tablette.jpg" alt="">
+            <div class="paragraphe">
+                <h2 class="para1">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam, iusto?</h2>
+                <h3 class="para2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia delectus vitae eaque fugit, accusamus itaque voluptatem assumenda doloribus suscipit enim.</h3>
+            </div>
+        </div>
+        </section>
+    <section class="c s2">
+        <div class="c-i">
+            <div class="carre1">
+            </div>
+            <img class="image1" src="images/cafe.jpg" alt="">
+        </div>
+        <div class="bloctext">
+            <h2 class="text2">TEXTE TEST</h2>
+            <h3 class="text2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam, fuga quo neque fugiat beatae autem adipisci unde quos sint illo natus ipsum error, quidem possimus quae? Perspiciatis aliquid quam quibusdam quia ex veniam ipsum ea minima? Exercitationem sequi nihil rerum eveniet doloribus enim odit ab quia maxime saepe quam libero ex ut debitis, necessitatibus eius? Adipisci impedit reiciendis cum. Distinctio obcaecati quia aspernatur ipsum velit asperiores in eum maiores rerum accusantium perferendis hic impedit adipisci ullam.</h3>
+        </div>
+    </section>
+    <section class="c s3">
+        <div class="bloctext1">
+            <h2 class="text3">TEXTE TEST</h2>
+            <h3 class="text3">Lorem ipsum dolor sit amet consectetur blabla adipisicing elit. Totam, fuga quo neque fugiat beatae autem adipisci unde quos sint illo natus ipsum error, quidem possimus quae? Perspiciatis aliquid quam quibusdam quia ex veniam ipsum ea minima? Exercitationem sequi nihil rerum eveniet doloribus enim odit ab quia maxime saepe quam libero ex ut debitis, necessitatibus eius? Adipisci impedit reiciendis cum. Distinctio obcaecati quia aspernatur ipsum velit asperiores in eum maiores rerum accusantium perferendis hic impedit adipisci ullam, blanditiis .</h3>
+        </div>
+        <div class="c-i2">
+            <div class="carre2">
+            </div>
+            <div class="carre3">   
+            </div>
+            <img class="image2" src="images/neige.jpg" alt="">
+        </div>
+    </section>
+    <?php include "Footer.php" ?>
+    <script src="fleche.js"></script>
+</body>
+</html>
