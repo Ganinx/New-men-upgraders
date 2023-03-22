9e2f652e9d7fa3a92ac569e2d8ebd25b24a41de6
Moonbrite <josemiguelvalecosta@gmail.com>
2023-03-22 15:22:04 +0100

    fix bug sur js cv manque fond noir

--- js/cv.js
+++ js/cv.js
@@ -1,10 +1,11 @@
 const image = document.getElementsByClassName('zoomable')[0];
-var body = document.getElementsByClassName('fond-noir')[0];
-var no = document.getElementById('no-scroll');
+const body = document.getElementsByClassName('fond-noir')[0];
+const no = document.getElementById('no-scroll');
 
-image.addEventListener('click', () => {
-    image.classList.toggle('zoom');
+document.querySelectorAll(".zoomable").forEach(image => {
+  image.addEventListener("click", () => {
+    image.classList.toggle("zoom");
     body.classList.toggle('body-noir');
     no.classList.toggle('bouge-pas');
+  });
 });
-
