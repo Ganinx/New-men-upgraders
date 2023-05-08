b0e9a054e7280cb0366b912d48e2e857c13a9127
Moonbrite <josemiguelvalecosta@gmail.com>
2023-05-08 13:57:25 +0200

    fix nos cv title

--- nos-CV.php
+++ nos-CV.php
@@ -11,7 +11,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>NewmenUprgraders/nos-CV</title>
     <link rel="stylesheet" href="CSS/main.css">
     <link rel="stylesheet" href="CSS/header.css">
     <link rel="stylesheet" href="CSS/footer.css">
