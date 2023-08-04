111c08f63809b13f9b3ecd9a0057b65daf27a760
valentin <delomenie-v-87@hotmail.fr>
2023-08-04 16:12:05 +0200

    starting version 2.0

--- js/fleche.js
+++
@@ -1,8 +0,0 @@
-var flechejaune = document.getElementsByClassName("flechejaune") [0];
-
-flechejaune.addEventListener("click",function() {
-    window.scroll({
-        top:0,
-        behavior:"smooth"
-    });
-})
